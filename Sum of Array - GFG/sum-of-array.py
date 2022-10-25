725daabf77232c0c83d30cb4be81378400320b24
harshberry <105843223+harshberry@users.noreply.github.com>
2022-10-25 23:38:09 +0530

    Added solution - LeetHub

---
+++ Sum of Array - GFG/sum-of-array.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+import numpy as np
+class Solution:
+
+	def _sum(self,arr, n):
+   		
+   		return sum(arr)
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+
+# Driver code 
+if __name__ == "__main__": 		
+    tc=int(input())
+    while tc > 0:
+        n=int(input())
+        arr=list(map(int , input().strip().split()))
+        ob = Solution()
+        ans = ob._sum(arr, n)
+        print(ans)
+        tc=tc-1
+# } Driver Code Ends
