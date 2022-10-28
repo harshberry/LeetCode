b414ce4346341ff4f232f2602921b5c785540004
harshberry <105843223+harshberry@users.noreply.github.com>
2022-10-28 16:12:29 +0530

    Added solution - LeetHub

---
+++ Print Elements of Array - GFG/print-elements-of-array.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+class Solution:
+    # Just print the space seperated array elements
+	def printArray(self,arr, n):
+	    for i in arr:
+	        print (i, end=' ')
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+# Driver code 
+if __name__ == "__main__": 		
+    tc=int(input())
+    while tc > 0:
+        n=int(input().strip())
+        arr=list(map(int, input().strip().split()))
+        ob = Solution()
+        ob.printArray(arr, n)
+        print()
+        tc=tc-1
+# } Driver Code Ends
