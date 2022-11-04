2fc57fa55386328f23f938efb1d690494aadffea
harshberry <105843223+harshberry@users.noreply.github.com>
2022-11-04 15:59:05 +0530

    Added solution - LeetHub

---
+++ Missing number - GFG/missing-number.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+def missingNumber( A, N):
+     return list(set(list(range(1,N+1)))-set(A))[0]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        print(missingNumber(a, n))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
